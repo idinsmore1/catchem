9177a1f1dbb816c8ab02c30917f2600263d3e869
Ian Dinsmore <iandinsmore1@gmail.com>
2024-10-03 00:33:03 -0400

    added config options

--- src/catchem/__init__.py
+++ src/catchem/__init__.py
@@ -1,25 +1,143 @@
 import argparse
-from catchem.logic import catch_em_all
+import json
+from catchem.logic import catch_em_all, _update_catchem_config, fuzzy_search
+from catchem.consts import lib_path, catchem_config, gen_map
 
 
 def catchem() -> None:
     """Main function for the catchem command line tool
     Will display the base stats of a pokemon in a tabulated format with color
     """
-    parser = argparse.ArgumentParser(description="Display the base stats of a Pokémon")
-    parser.add_argument("pokemon", nargs='+', action=OneToThreeArgAction, help="The pokemon to display")
-    parser.add_argument("-g", "--game", help="The game version to display the moves for", default=None)
+    parser_desc = (
+        """Catch'em will show you everything you need to know about a Pokémon"""
+    )
+    parser = argparse.ArgumentParser(description=parser_desc)
+    parser.add_argument(
+        "pokemon", nargs="+", action=OneToThreeArgAction, help="The pokemon to display"
+    )
+    parser.add_argument(
+        "-g", "--game", nargs='+', help="The game version to display the moves for", default=None
+    )
+    parser.add_argument(
+        "-hs", "--hide-stats", help="Hide the stats table", action="store_true"
+    )
+    parser.add_argument(
+        "-hm", "--hide-moves", help="Hide the moves table", action="store_true"
+    )
+    parser.add_argument(
+        "-he", "--hide-evos", help="Hide evolutions", action="store_true"
+    )
+    parser.add_argument(
+        "-ss", "--show-stats", help="Show the stats table", action="store_true"
+    )
+    parser.add_argument(
+        "-sm", "--show-moves", help="Show the moves table", action="store_true"
+    )
+    parser.add_argument(
+        "-se", "--show-evos", help="Show evolutions", action="store_true"
+    )
     args = parser.parse_args()
-    pokemon = ' '.join(args.pokemon).lower()
-    catch_em_all(pokemon)
+    pokemon = " ".join(args.pokemon).lower()
+    config = _temp_update_config(args)
+    catch_em_all(pokemon, config)
 
 
+def catchem_how() -> None:
+    "Update the catchem config file"
+    parser = argparse.ArgumentParser(description="Update the catchem config file")
+    parser.add_argument(
+        "-g",
+        "--game",
+        nargs="+",
+        help="The game version to display the moves for",
+        default='scarlet',
+    )
+    parser.add_argument(
+        "-hs", "--hide-stats", help="Hide the stats table", action="store_true"
+    )
+    parser.add_argument(
+        "-hm", "--hide-moves", help="Hide the moves table", action="store_true"
+    )
+    parser.add_argument(
+        "-he", "--hide-evos", help="Hide evolutions", action="store_true"
+    )
+    parser.add_argument(
+        "-ss", "--show-stats", help="Show the stats table", action="store_true"
+    )
+    parser.add_argument(
+        "-sm", "--show-moves", help="Show the moves table", action="store_true"
+    )
+    parser.add_argument(
+        "-se", "--show-evos", help="Show evolutions", action="store_true"
+    )
+
+    args = parser.parse_args()
+    if isinstance(args.game, list):
+        args.game = "-".join(args.game).lower()
+    if args.show_stats:
+        if args.hide_stats:
+            raise argparse.ArgumentError(
+                argument=None,
+                message="You can't show (--show-stats) and hide (--hide-stats) at the same time"
+            )
+        args.hide_stats = not args.show_stats
+    if args.show_moves:
+        if args.hide_moves:
+            raise argparse.ArgumentError(
+                argument=None,
+                message="You can't show (--show-moves) and hide (--hide-moves) at the same time"
+            )
+        args.hide_moves = not args.show_moves
+    if args.show_evos:
+        if args.hide_evos:
+            raise argparse.ArgumentError(
+                argument=None,
+                message="You can't show (--show-evos) and hide (--hide-evos) at the same time"
+            )
+        args.hide_evos = not args.show_evos
+    _update_catchem_config(
+        args.game, args.hide_stats, args.hide_moves, args.hide_evos
+    )
+
+
+def _temp_update_config(args):
+    temp_config = catchem_config.copy()
+    if args.game is not None:
+        if isinstance(args.game, list):
+            args.game = "-".join(args.game).lower()
+        game, score = fuzzy_search(args.game, list(gen_map.keys()))
+        if score < 70:
+            print("Error: Game could not be found")
+            return
+        
+        game_gen = gen_map[game]
+        temp_config["game"] = game
+        temp_config["gen"] = game_gen
+    else:
+        game = temp_config["game"]
+    print(f"Showing data for Pokémon {game.title()}")
+    if args.hide_stats:
+        temp_config["hide_stats"] = True
+    if args.show_stats:
+        temp_config["hide_stats"] = False
+    if args.hide_moves:
+        temp_config["hide_moves"] = True
+    if args.show_moves:
+        temp_config["hide_moves"] = False
+    if args.hide_evos:
+        temp_config["hide_evos"] = True
+    if args.show_evos:
+        temp_config["hide_evos"] = False
+    return temp_config
 
 class OneToThreeArgAction(argparse.Action):
     """Custom action for argparse to handle 1-3 arguments"""
+
     def __call__(self, parser, namespace, values, option_string=None):
         if len(values) < 3:
-            values += [''] * (3 - len(values))
+            values += [""] * (3 - len(values))
         elif len(values) > 3:
-            raise argparse.ArgumentError(self, "Woah there trainer! A pokemon's name can only be 1-3 words long.")
+            raise argparse.ArgumentError(
+                self, "Woah there trainer! A pokemon's name can only be 1-3 words long."
+            )
         setattr(namespace, self.dest, values)
--- src/catchem/logic.py
+++ src/catchem/logic.py
@@ -1,52 +1,96 @@
+import json
 from fuzzywuzzy import fuzz, process
-from catchem.consts import pokestats, colors
+from catchem.consts import pokestats, colors, lib_path, gen_map, catchem_config
 from tabulate import tabulate
-
-def fuzzy_search(query, limit=5):
-    choices = list(pokestats.keys())
-    return process.extractOne(query, choices, scorer=fuzz.token_sort_ratio)
+from pprint import pprint
 
 
-def catch_em_all(pokemon):
+def catch_em_all(pokemon, config):
     """Get base stats, moves, and evolutions of a pokemon
     :param pokemon: The name of the pokemon
+    :param config: The config holding the game version and other data
     """
-    pokemon, score = fuzzy_search(pokemon)
+    gen = config.get("gen")
+    show_stats = not config.get("hide_stats")
+    show_moves = not config.get("hide_moves")
+    show_evos = not config.get("hide_evos")
+
+    poke_choices = list(pokestats.keys())
+    pokemon, score = fuzzy_search(pokemon, poke_choices)
     if score < 70:  # If the score is less than 70, the match is too weak
         print(f"Error: {pokemon.title()} not found")
         pokemon = input("Try again: ")
         catch_em_all(pokemon)
         return
     
+    if all([not show_stats, not show_moves, not show_evos]):
+        print(f"I guess you and {pokemon.title()} haven't reached 255 friendship yet? 🤔")
+        print("If you didn't use '-hs', '-hm', and '-he' all at once, use catchem-how to update your global config")
+        print(f"current global config: {catchem_config}")
+        return
+
+
     stats = pokestats[pokemon]
-    print(f"Base stats for {pokemon.title()}")
-    print(make_stats_table(stats))
+    # Show Base Stats
+    stats_table = make_stats_table(stats)
+    print(stats_table)
+    # Show Moves
 
 
 def make_stats_table(stats):
-    pokemon = stats['name']
-    dex_no = stats['dex_no']
-    poke_types = stats['types']
+    pokemon = stats["name"]
+    dex_no = stats["dex_no"]
+    poke_types = stats["types"]
     stat_names = zip(
-        ['hp', 'attack', 'defense', 'spatt', 'spdef', 'speed', 'total'],
-        ['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Total']
+        ["hp", "attack", "defense", "spatt", "spdef", "speed", "total"],
+        ["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed", "Total"],
     )
     stat_rows = []
     for stat_tag, stat_name in stat_names:
         stat_value = stats[stat_tag]
-        if stat_tag == 'total':
-            rgb_info  = _total_stat_code(stat_value)
+        if stat_tag == "total":
+            rgb_info = _total_stat_code(stat_value)
             color_bar = rgb_text(*rgb_info)
             stat_rows.append([stat_name, stat_value, color_bar])
             continue
         color = _base_stat_code(stat_value)
-        stat_bar = '|' * (stat_value // 2)
+        stat_bar = "|" * (stat_value // 2)
         color_bar = rgb_text(stat_bar, color)
         stat_rows.append([stat_name, stat_value, color_bar])
-    type_header_string = ' '.join([rgb_text(f' {type_.title()} ', type_) for type_ in poke_types])
-    header = [f'No: {dex_no}', pokemon.title(), type_header_string]
-    return tabulate(stat_rows, headers=header, tablefmt='mixed_grid')
-    
+    type_header_string = " ".join(
+        [rgb_text(f" {type_.title()} ", type_) for type_ in poke_types]
+    )
+    header = [f"No: {dex_no}", pokemon.title(), type_header_string]
+    return tabulate(stat_rows, headers=header, tablefmt="mixed_grid")
+
+
+## Catchem How Functions
+
+
+def _update_catchem_config(game, hide_stats, hide_moves, hide_evos):
+    config_file = lib_path / "config.json"
+    game_choices = list(gen_map.keys())
+    new_game, score = fuzzy_search(game, game_choices)
+    if score < 70:
+        print(f"Error: {game.title()} not found")
+        game = input("Enter a valid Pokemon game: ")
+        _update_catchem_config(game, hide_stats, hide_moves, hide_evos)
+        return
+    game_gen = gen_map[new_game]
+    new_config = {
+        "game": new_game,
+        "gen": game_gen,
+        "hide_stats": hide_stats,
+        "hide_moves": hide_moves,
+        "hide_evos": hide_evos,
+    }
+    print("New Config:")
+    pprint(new_config)
+    with config_file.open("w") as f:
+        json.dump(new_config, f, indent=4)
+
+
+## Utility Functions
 
 
 def rgb_text(text, color, background=True):
@@ -57,22 +101,32 @@ def rgb_text(text, color, background=True):
 
 def _base_stat_code(stat_val):
     if stat_val < 50:
-        return 'red'
+        return "red"
     elif stat_val < 90:
-        return 'yellow'
+        return "yellow"
     elif stat_val < 125:
-        return 'green'
+        return "green"
     else:
-        return 'blue'
-    
+        return "blue"
+
+
 def _total_stat_code(stat_val) -> tuple[str, str]:
     if stat_val < 350:
-        return ' Bad ', 'red'
+        return " Bad ", "red"
     elif stat_val < 450:
-        return ' Mid ', 'yellow'
+        return " Mid ", "yellow"
     elif stat_val < 500:
-        return ' Good ', 'green'
+        return " Good ", "green"
     elif stat_val < 600:
-        return ' Great ', 'grass'
+        return " Great ", "grass"
     else:
-        return ' Legend/Pseudo ', 'blue'
+        return " Legend/Pseudo ", "blue"
+
+
+def _is_regional(name_list):
+    regions = ["alolan", "hisuian", "galarian", "paldean"]
+    return any(region in name_list for region in regions)
+
+
+def fuzzy_search(query, choices):
+    return process.extractOne(query, choices, scorer=fuzz.token_sort_ratio)
